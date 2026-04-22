01137f44631b4529271229d95472d0b2ba07db11
raq1409 <raquel201311@gmail.com>
2026-04-22 14:25:52 +0200

    Exercici 6

--- ej4a6/src/main/java/MergeSortClients.java
+++ ej4a6/src/main/java/MergeSortClients.java
@@ -67,18 +67,61 @@ public class MergeSortClients {
 
     // This function recursively divides the array and merges the sorted parts
     public static void mergeSort(int[] arr, int left, int right) {
-        // TODO: Implement recursive division and call merge
+        if(left < right) {
+            int center = left + (right - left) / 2;
+
+            mergeSort(arr, left, center);
+            mergeSort(arr, center + 1, right);
+            merge(arr, left, center, right);
+        }
     }
 
     // This function merges two sorted subarrays
     public static void merge(int[] arr, int left, int mid, int right) {
-        // TODO: Create temporary arrays and merge them in sorted order
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for(int i = 0; i < n1; i++) {
+            leftArr[i] = arr[left + i];
+        }
+
+        for(int i = 0; i < n2; i++) {
+            rightArr[i] = arr[mid + 1 + i];    
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if(leftArr[i] <= rightArr[j]){
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        } 
     }
 
     // -------------------------------------------------------------
     // Manual test using IDE
     // -------------------------------------------------------------
-    /*
+    
     public static void main(String[] args) {
         int[] tiempos = {12, 5, 8, 3, 15, 7, 2, 10, 6, 4};
         mergeSort(tiempos, 0, tiempos.length - 1);
@@ -88,7 +131,7 @@ public class MergeSortClients {
             System.out.print(t + " ");
         }
     }
-    */
+    
 	// Torna a comentar aquest main quan vulguis executar els tests amb maven test
     // Vuelve a comentar este main cuando quieras ejecutar los tests con:
     // mvn test
